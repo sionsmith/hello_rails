bd7be01b17f17896bc49048cd2ae321494ff141f
Marc Chamberland <chamberland.marc@gmail.com>
2020-03-12 20:30:23 -0400

    WIP add helm chart
    Helm is de-facto standard for packaging K8S apps, and TF can wrap
    it so helm's status will be available in TF. It currently deploys
    the hello_rails app, but it's not serving it's content yet

--- tf_app/main.tf
+++ tf_app/main.tf
@@ -1,59 +1,12 @@
-# Setup provider
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.9"
-}
-
-# Bring up some pods
-resource "kubernetes_pod" "hello_rails" {
-  metadata {
-    name = "hello_rails"
-  }
-
-  spec {
-    container {
-      image = "hello_rails:1.7.9"
-      name  = "hello_rails"
-
-      env {
-        name  = "environment"
-        value = "test"
-      }
-
-      liveness_probe {
-        http_get {
-          path = "/nginx_status"
-          port = 80
-
-          http_header {
-            name  = "X-Custom-Header"
-            value = "Awesome"
-          }
-        }
-
-        initial_delay_seconds = 3
-        period_seconds        = 3
-      }
-    }
-
-    dns_config {
-      nameservers = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
-      searches    = ["example.com"]
-
-      option {
-        name  = "ndots"
-        value = 1
-      }
-
-      option {
-        name = "use-vc"
-      }
-    }
-
-    dns_policy = "None"
+# Setup provider. We wrap helm in TF so we ca have it's data in tfstate
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    load_config_file       = false
+    version                = "~> 1.9"
   }
 }
+
 
